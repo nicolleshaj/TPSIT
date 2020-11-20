59b9e7eb2e61b60367470f4d910bf1d2cf8dedab
nicolleshaj <71812836+nicolleshaj@users.noreply.github.com>
2020-11-20 17:14:39 +0100

    Add files via upload

---
+++ python/poligono.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+tartaruga = Turtle()
+sfondo = Screen()
+
+sfondo.bgcolor("yellow")
+
+tartaruga.pencolor("red")
+
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+tartaruga.right(45)
